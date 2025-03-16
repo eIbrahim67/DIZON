3f2662a12f078a676dce63afd345e3951389d04b
Ibrahim-M-I <legion5.15ith6h@gmail.com>
2025-03-16 06:50:54 +0200

    Create ChangePassword Screen and update EditProfile UI

---
+++ app/src/main/java/com/eibrahim/dizon/change_password/ChangePasswordFragment.kt
@@ -0,0 +1,31 @@
+package com.eibrahim.dizon.change_password
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eibrahim.dizon.R
+
+class ChangePasswordFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ChangePasswordFragment()
+    }
+
+    private val viewModel: ChangePasswordViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_change_password, container, false)
+    }
+}
---
+++ app/src/main/java/com/eibrahim/dizon/change_password/ChangePasswordViewModel.kt
@@ -0,0 +1,7 @@
+package com.eibrahim.dizon.change_password
+
+import androidx.lifecycle.ViewModel
+
+class ChangePasswordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
