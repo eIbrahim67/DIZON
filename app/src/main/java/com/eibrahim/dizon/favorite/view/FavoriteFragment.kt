8c79e0f6bd0eca5c3569d8f0d5e5a64d1f675be4
MoamenAbdelrhman <moamen816@outlook.com>
2025-04-16 14:52:51 +0200

    update UI of FavouriteFragment

--- app/src/main/java/com/eibrahim/dizon/favorite/view/FavoriteFragment.kt
+++ app/src/main/java/com/eibrahim/dizon/favorite/view/FavoriteFragment.kt
@@ -1,11 +1,13 @@
 package com.eibrahim.dizon.favorite.view
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eibrahim.dizon.R
 import com.eibrahim.dizon.favorite.viewModel.FavoriteViewModel
@@ -14,27 +16,35 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class FavoriteFragment : Fragment() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var favoriteAdapter: AdapterRVFavorite
+    private val viewModel: FavoriteViewModel by viewModels()
 
     companion object {
         fun newInstance() = FavoriteFragment()
     }
 
-    private val viewModel: FavoriteViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_favourit, container, false)
+        val view = inflater.inflate(R.layout.fragment_favourit, container, false)
 
+        recyclerView = view.findViewById(R.id.recyclerViewFavorite)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
+        favoriteAdapter = AdapterRVFavorite(emptyList())
+        recyclerView.adapter = favoriteAdapter
 
+        // Observe the LiveData from ViewModel
+        viewModel.favoriteProperties.observe(viewLifecycleOwner, Observer { favoriteList ->
+            favoriteAdapter.updateList(favoriteList)
+        })
 
+        return view
     }
 }
