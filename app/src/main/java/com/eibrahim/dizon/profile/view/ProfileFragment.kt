370ba3b16bd5049534c9740dd33e04e8e1e74225
MoamenAbdelrhman <moamen816@outlook.com>
2025-04-16 14:00:04 +0200

    add Profile UI

---
+++ app/src/main/java/com/eibrahim/dizon/profile/view/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.eibrahim.dizon.profile.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.eibrahim.dizon.R
+import com.google.android.material.button.MaterialButton
+
+class ProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_profile, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
