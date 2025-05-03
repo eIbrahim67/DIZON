b59ec8612cacdb8b0ad32430e79a8694e3588796
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2025-05-04 01:29:11 +0300

    connect Search with our back end api

---
+++ app/src/main/java/com/eibrahim/dizon/search/api/SearchApi.kt
@@ -0,0 +1,27 @@
+package com.eibrahim.dizon.search.api
+
+import com.eibrahim.dizon.search.data.SearchPropertyResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SearchApi {
+    @GET("/api/Properties/GetAll")
+    suspend fun getAllProperties(
+        @Query("PropertyType") propertyType: String?,
+        @Query("City") city: String?,
+        @Query("Governate") governate: String?,
+        @Query("Bedrooms") bedrooms: Int?,
+        @Query("Bathrooms") bathrooms: Int?,
+        @Query("SortBy") sortBy: String?,
+        @Query("Size") size: Double?,
+        @Query("MaxPrice") maxPrice: Double?,
+        @Query("MinPrice") minPrice: Double?,
+        @Query("InternalAmenityIds") internalAmenityIds: List<Int>?,
+        @Query("ExternalAmenityIds") externalAmenityIds: List<Int>?,
+        @Query("AccessibilityAmenityIds") accessibilityAmenityIds: List<Int>?,
+        @Query("PageSize") pageSize: Int,
+        @Query("PageIndex") pageIndex: Int
+    ): Response<SearchPropertyResponse>
+
+}
